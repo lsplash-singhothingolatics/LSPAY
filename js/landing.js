b0b2f8061b99db040d66a8dd0e22999e675348a3
LSPLASH-INDIA <nehabhardwaj231085@gmail.com>
2026-08-18 19:43:42 +0530

    Delete js directory

--- js/landing.js
+++
@@ -1,8 +0,0 @@
-// If already logged in, send the primary CTA straight to the dashboard.
-(function () {
-  var cta = document.getElementById('ctaOpen');
-  if (cta && window.LSPAY && LSPAY.currentUser()) {
-    cta.textContent = 'Go to dashboard';
-    cta.setAttribute('href', 'dashboard.html');
-  }
-})();
